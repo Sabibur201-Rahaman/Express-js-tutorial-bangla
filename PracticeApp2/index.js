45d8888a60c0ef5a49c15c71662bf0202d99d231
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-11-27 19:20:16 +0530

    simple get request implemented in index

--- PracticeApp2/index.js
+++ PracticeApp2/index.js
@@ -2,27 +2,30 @@ var express=require('express');
 var multer=require('multer')
 var app=express();
 
+app.get('/',function(res,res){
+    res.send('this is simple  get request ')
+})
 
-var storage=multer.diskStorage({
-    destination:function (req,file,callBack) {
-        callBack(null,'./uploads');
-    },
-    filename:function (req,file,callBack) {
-        callBack(null,file.originalname)
-    }
-});
+// var storage=multer.diskStorage({
+//     destination:function (req,file,callBack) {
+//         callBack(null,'./uploads');
+//     },
+//     filename:function (req,file,callBack) {
+//         callBack(null,file.originalname)
+//     }
+// });
 
-var upload=multer({storage:storage}).single('myfile')
-app.post('/',function (req,res) {
-    upload(req,res,function (error) {
-            if(error){
-                res.send("File Upload Fail")
-            }
-            else{
-                res.send("File Upload Success")
-            }
-    });
-});
+// var upload=multer({storage:storage}).single('myfile')
+// app.post('/',function (req,res) {
+//     upload(req,res,function (error) {
+//             if(error){
+//                 res.send("File Upload Fail")
+//             }
+//             else{
+//                 res.send("File Upload Success")
+//             }
+//     });
+// });
 
 
 app.listen(8000,function () {
