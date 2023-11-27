cdce94a4084867aa2f09573c4af2b75007ae391b
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-11-27 23:34:53 +0530

    working with get request header

--- PracticeApp2/index.js
+++ PracticeApp2/index.js
@@ -2,10 +2,23 @@ var express=require('express');
 var multer=require('multer')
 var app=express();
 
-app.get('/',function(res,res){
-    res.send('this is simple  get request ')
-})
+// app.get('/',function(res,res){
+//     res.send('this is simple  get request ')
+// })
 
+// app.get('/',function(req,res){
+//     let firstName=req.query.firstName;
+//     let lastName=req.query.lastName;
+
+//     res.end(firstName +" "+lastName)
+// })
+
+app.get('/',function(req,res){
+    let firstName=req.header('firstName');
+    let lastName=req.header('lastName');
+
+    res.end(firstName +" "+lastName)
+})
 // var storage=multer.diskStorage({
 //     destination:function (req,file,callBack) {
 //         callBack(null,'./uploads');
